239503776c1626184a838a426c9ce4fdbe0632b4
arthur <arthur@ubuntu.ubuntu-domain>
2011-10-12 11:28:27 -0400

    add

--- microsoft/findMid.cc
+++ microsoft/findMid.cc
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <list>
+#include <iterator>
+#include <algorithm>
 
 using namespace std;
 
-void findMid(int *seq)
+int findMid(list<int> l)
 {
-    int *p = seq;
+    list<int>::iterator cur = l.begin(), mid = l.begin();
+    while(cur++ != l.end())
+    {
+        if(cur != l.end())
+        {
+            ++cur;
+            if(cur != l.end())
+                ++mid;
+        }
+    }
+    return *mid;
+}
+
+void print(int i)
+{
+    cout << i << " ";
 }
 int main()
 {
+    int arr[] = {2, 34, 1, 5,  9, 2, 2};
+    list<int> l(arr, arr+sizeof(arr)/sizeof(int));
+    for_each(l.begin(), l.end(), print);
+    cout << endl;
+
+    cout << findMid(l) << endl;
 
     return 0;
 }
