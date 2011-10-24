b9eda6be1e0110a2872af366e82d4060a0d45b82
arthur <arthur@ubuntu.ubuntu-domain>
2011-10-23 20:46:58 -0400

    add

--- git.sh
+++ git.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull git@github.com:arthurbryant/job-hunting.git
 git add .
 git commit -m "add"
 git push
--- microsoft/a.cc
+++ microsoft/a.cc
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+void print(int i)
+{
+    cout << i << " ";
+}
 int main()
 {
-    int n;
-    cin >> n;
-    cout << (!!n) << endl;
+    const int max = 256;
+    int arr[max];
+    for(int i = 0; i < max; ++i)
+        arr[i] = 0;
+    arr['a'] = 2;
+    arr['b'] = 2;
+    for_each(arr, arr+sizeof(arr)/sizeof(int), print);
 
     return 0;
 }
